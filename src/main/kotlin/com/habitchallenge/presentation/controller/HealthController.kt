a6c22c638bed9a844174cd20f5f8ac8d87f6eebe
YASICJUNWOO <joonoo3@naver.com>
2025-09-20 23:08:45 +0900

    fix: correct PostgreSQL dialect spelling in application-prod.yml and clean up HealthController annotation

--- src/main/kotlin/com/habitchallenge/presentation/controller/HealthController.kt
+++ src/main/kotlin/com/habitchallenge/presentation/controller/HealthController.kt
@@ -3,7 +3,7 @@ package com.habitchallenge.presentation.controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController()
+@RestController
 @RequestMapping("/health-check")
 class HealthController {
 
